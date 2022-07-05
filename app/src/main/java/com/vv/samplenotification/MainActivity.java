6b2e8515dfb884729ee02240b4b44f5cc44cd9f0
hero4127 <vishalpatole2000@gmail.com>
2022-07-05 19:04:36 +0530

    Initial

---
+++ app/src/main/java/com/vv/samplenotification/MainActivity.java
@@ -0,0 +1,70 @@
+package com.vv.samplenotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.button);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent notificationIntent = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent contentIntent = PendingIntent.getActivity(MainActivity.this,
+                        101, notificationIntent,
+                        PendingIntent.FLAG_CANCEL_CURRENT);
+
+                NotificationManager nm = (NotificationManager) MainActivity.this
+                        .getSystemService(Context.NOTIFICATION_SERVICE);
+                String NOTIFICATION_CHANNEL_ID = "tutorialspoint_01";
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    @SuppressLint("WrongConstant") NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_MAX);
+                    // Configure the notification channel.
+                    notificationChannel.setDescription("Sample Channel description");
+                    notificationChannel.enableLights(true);
+                    notificationChannel.setLightColor(Color.RED);
+                    notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+                    notificationChannel.enableVibration(true);
+                    nm.createNotificationChannel(notificationChannel);
+                }
+
+                NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(MainActivity.this, NOTIFICATION_CHANNEL_ID);
+                notificationBuilder.setAutoCancel(true)
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setTicker("Tutorialspoint")
+                        //.setPriority(Notification.PRIORITY_MAX)
+                        .setContentTitle("sample notification")
+                        .setContentText("This is sample notification")
+                        .setContentInfo("Information");
+
+                nm.notify(1, notificationBuilder.build());
+            }
+        });
+
+    }
+}
